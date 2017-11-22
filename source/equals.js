7265b7bdfb35a76e7753216f728450fec6179987
Michael Hurley <mh@buzzdecafe.com>
2017-11-22 15:08:57 -0500

    warned

---
+++ source/equals.js
@@ -0,0 +1,35 @@
+import _curryN from './internal/_curryN';
+import _equals from './internal/_equals';
+
+
+/**
+ * Returns `true` if its arguments are equivalent, `false` otherwise. Handles
+ * cyclical data structures.
+ *
+ * Dispatches symmetrically to the `equals` methods of both arguments, if
+ * present.
+ *
+ * @func
+ * @memberOf R
+ * @since v0.15.0
+ * @category Relation
+ * @sig a -> b -> Boolean
+ * @param {*} a
+ * @param {*} b
+ * @return {Boolean}
+ * @example
+ *
+ *      R.equals(1, 1); //=> true
+ *      R.equals(1, '1'); //=> false
+ *      R.equals([1, 2, 3], [1, 2, 3]); //=> true
+ *
+ *      var a = {}; a.v = a;
+ *      var b = {}; b.v = b;
+ *      R.equals(a, b); //=> true
+ */
+function __equals(a, b) {
+  return _equals(a, b, [], []);
+}
+equals.sig = ['a -> b -> Boolean'];
+
+export default _curryN(2, __equals);
